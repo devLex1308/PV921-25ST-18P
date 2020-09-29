6582f6244b8e78bcb88fb7a2d12677b962716a29
Студент <student@ITStep.local>
2020-09-29 19:01:17 +0300

    Завдання №1 4 уроку

--- lessons/04/tools.js
+++ lessons/04/tools.js
@@ -1,3 +1,25 @@
-function log(text) {
-  console.log(text);
+let number = 0;
+
+function plus(a){
+	number += a;
+	console.log(number);
+}
+
+function minus(a){
+	number -= a;
+	console.log(number);
+}
+
+function fre(){
+	number *= 2;
+	console.log(number);
+}
+
+function div(){
+	number /= 2;
+	console.log(number);
+}
+
+function log(){
+	console.log(number);
 }
