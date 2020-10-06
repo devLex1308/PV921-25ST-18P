cca7a55da9c9333d73602b23f3afb42d523336fb
Taras Nedzeldkyi <nedzelskyitaras@gmail.com>
2020-10-06 20:30:39 +0300

    Taras

--- lessons/06/tools.js
+++ lessons/06/tools.js
@@ -1,173 +1,177 @@
-
-
 const userNameElement = document.querySelector("#userName");
 const userSemameElement = document.querySelector("#userSemame");
 const userMiddlenameElement = document.querySelector("#userMiddlename");
-
 const userPhoneElement = document.querySelector("#userPhone");
-const userIgreeElement = document.querySelector("#userIgree");
+const userAgreeElement = document.querySelector("#userAgree");
 
 const userBusElement = document.querySelector("#userBus");
 const userHotelElement = document.querySelector("#userHotel");
-const userTextElement = document.querySelector("#userText");
+
 
 const buttons = document.querySelectorAll(".goToStep");
 
-document.querySelector("#step3").classList.add("show");
+const finalButton = document.querySelector("button");
 
-for(let i = 0; i < buttons.length; i++) {
-  const button = buttons[i];
-  button.addEventListener("click", clickButton)
+console.log(finalButton);
+
+document.querySelector('#step3').classList.add("show");
+
+for (let i = 0; i < buttons.length; i++) {
+	const button = buttons[i];
+	button.addEventListener("click", clickButton);
 }
-
 
 function clickButton(event) {
-  console.log(event);
-  const button = event.target;
-  console.log({button});
-  const step = event.target.getAttribute("data-go-to-step");
-   
-  const submit = event.target.getAttribute("data-submit");
+	console.log(event);
+	const button = event.target;
+	console.log({button});
+	const step = event.target.getAttribute("data-go-step");
 
-  if (submit) {
-    checkStep3();
-    console.log(getAllData());
-    resetData();
-  }
-  // console.log({step});
+	const submit = event.target.getAttribute("data-submit");
 
-  if (step && check(step)){
-    // showStep(step);
-    console.log("Все гаразд");
-  }
+	
 
+	console.log(submit);
+
+	if(submit){
+		checkStep3();
+	}
+
+	//console.log({step});
+
+	// console.log(userSemameElement.value);
+	// console.log(userNameElement.value);
+	// console.log(userMiddlenameElement.value);
+	// console.log(userPhoneElement.value);
+	// console.log(userAgreeElement.checked);
+
+
+
+//////////////////////DZ////////////////////////
+
+	if(step && checkData(step)){
+		showStep(step);
+	}
+
+	//console.log(checkData(step));
+/////////////////////////////////////////////
+	
 }
 
-function check(step) {
-
-  if (step == 2) {
-    return checkStep1();
-  }
-
-  if (step == 3) {
-    return checkStep2();
-  }
-
-  return false;
-}
-
-function checkStep1() {
-
-  const name = userNameElement.value;
-  const sename = userSemameElement.value;
-  const middleName = userMiddlenameElement.value;
-  
-  const phone = userPhoneElement.value;
-  const iGree = userIgreeElement.checked;
-
-  if (!checkInputText(name)) {
-    alert("Ім'я ввдено не вірно");
-    return false;
-  }
-
-  if (!checkInputText(sename)) {
-    alert("Прізвище ввдено не вірно");
-    return false;
-  }
-
-  if (!checkInputText(middleName)) {
-    alert("По батькові ввдено не вірно");
-    return false;
-  }
-
-  if (!checkPhone(phone)) {
-    alert("Телефон ввдено не вірно");
-    return false;
-  }
-
-  if(!iGree) {
-    alert("Ви повинні погодитись на обробку персональних даних!");
-    return false;
-  }
-
-  return true;
-
-}
-
-function resetData() {
-  userNameElement.value = "";
-  userSemameElement.value = "";
-  userMiddlenameElement.value = "";
-  userPhoneElement.value = "";
-  userIgreeElement.checked = false;
-  userBusElement.value = "1";
-  userHotelElement.value = "1";
-  document.querySelector("#step3 input[type='radio']:checked").checked = false;
-  userTextElement.value = "";
-}
-
-function checkStep2() {
-  const bus = userBusElement.value;
-  const hotel = userHotelElement.value;
-
-  console.log({
-    bus,
-    hotel,
-  })
-
-  return true;
-}
-
-function getAllData() {
-  const name = userNameElement.value;
-  const sename = userSemameElement.value;
-  const middleName = userMiddlenameElement.value;
-  const phone = userPhoneElement.value;
-  const iGree = userIgreeElement.checked;
-  const bus = userBusElement.value;
-  const hotel = userHotelElement.value;
-  const skyType = document.querySelector("#step3 input[type='radio']:checked").value;
-  const userText = userTextElement.value;
-
-  return {
-    name,
-    sename,
-    middleName,
-    phone,
-    iGree,
-    bus,
-    hotel,
-    skyType,
-    userText
-  }
+function showStep(step){
+	document.querySelector(".show").classList.remove("show");
+	document.querySelector(`#step${step}`).classList.add("show");
 }
 
 
-function checkStep3() {
-  console.log("checkStep3");
-  const radioElemen = document.querySelector("#step3 input[type='radio']:checked");
-  if (radioElemen) {
-    console.log(radioElemen.value);
-  }
+//////////////////////DZ////////////////////////
+
+
+function checkData(step){
+	switch(step){
+		case("2"):
+		return checkStep1();
+		case("3"):
+		return checkStep2();
+		default:
+		return true //false; 
+	}
+}
+
+function checkStep1(){
+	const rxName = /^[А-ЯЁЇІҐ]+[а-яёїієґ\-\']{1,39}$/;
+	const rxPhone = /^\+?\d{9,12}$/;
+
+	const res1 = checkInput(userNameElement, rxName);
+	const res2 = checkInput(userSemameElement, rxName);
+	const res3 = checkInput(userMiddlenameElement, rxName);
+	const res4 = checkInput(userPhoneElement, rxPhone);
+	const res5 = checkCheckBox(userAgreeElement);
+
+	return res1 && res2 && res3 && res4 && res5;
 }
 
 
+function checkStep2(){
+	
+	const bus = userBusElement.value;
+	const hotel = userHotelElement.value;
 
-function checkPhone(str) {
-  const rx = /^\+?\d{9,12}$/;
-  return rx.test(str);
+	// console.log(bus);
+	// console.log(hotel);
 }
 
-console.log(checkPhone("+380661234567"));
+function checkStep3(){
+	
+	const radioButtonElement = document.querySelector("#step3 input[type=radio]:checked");
+	//console.log(radioButtonElement);
 
-function checkInputText(str) {
-  const rx = /^[А-Яа-яёЁЇїІіЄєҐґ]{2,40}$/;
-  return rx.test(str);
 }
 
-function showStep(step) {
-  document.querySelector(".show").classList.remove("show");
-  document.querySelector(`#step${step}`).classList.add("show");
+
+function getAllData(){
+	const surname = userSemameElement.value;
+	const name = userNameElement.value;
+	const midleName = userMiddlenameElement.value;
+	const phone = userPhoneElement.value;
+	const agree = userAgreeElement.checked;
+	const bus = userBusElement.value;
+	const hotel = userHotelElement.value;
+	const abonem = document.querySelector("#step3 input[type=radio]:checked")
+	const qustion = document.querySelector('#userText');
+
+	return {
+		surname,
+		name,
+		midleName,
+		phone,
+		agree,
+		bus,
+		hotel,
+		abonem,
+		qustion,
+	}
 }
 
-// console.log(button.getAttribute("data-go-to-step"));
+	function resetData(){
+		// userSemameElement.value;
+		// userNameElement.value
+		// userMiddlenameElement.value
+		// userPhoneElement.value
+		// userAgreeElement.checked
+		// userBusElement.value
+		// userHotelElement.value
+		// document.querySelector("#step3 input[type=radio]:checked")
+		// document.querySelector('#userText');
+	}
+
+function checkInput(element, rx){
+	if(rx.test(element.value)){
+		if(element.classList.contains("red-border")){
+			element.classList.remove("red-border")
+		}
+		return true;
+	}
+	else{
+		if(!element.classList.contains("red-border")){
+			element.classList.add("red-border")
+		}
+		return false;
+	}
+}
+
+
+function checkCheckBox(element){
+	if(element.checked){
+		if(element.classList.contains("red-shadow")){
+			element.classList.remove("red-shadow")
+		}
+		return true;
+	}
+	else{
+		if(!element.classList.contains("red-shadow")){
+			element.classList.add("red-shadow")
+		}
+		return false;
+	}
+}
