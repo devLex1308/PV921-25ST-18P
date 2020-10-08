0fbe2865baa3a4196d6aadb6c69898be09ff6b8e
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-08 20:00:06 +0300

    Додано функції для ознайомлення з асинхронністю

--- lessons/08/tools.js
+++ lessons/08/tools.js
@@ -2,6 +2,26 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+const sleep1 = getRandomInt(5000);
+const sleep2 = getRandomInt(5000);
+const sleep3 = getRandomInt(5000);
+
+setTimeout(() =>{
+  console.log("Request 1");
+}, sleep1);
+
+function request1(){
+  console.log("Request 1");
+}
+
+function request2(){
+  console.log("Request 1");
+}
+
+function request3(){
+  console.log("Request 1");
+}
+
 
 
 async function getData() {
