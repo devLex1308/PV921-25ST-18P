6c2adfff1fdcfa71570189b3d74ce553916eb9e2
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-08 18:26:31 +0300

    Додано файли для роботи з запитами до сервера

---
+++ lessons/08/tools.js
@@ -0,0 +1,11 @@
+
+
+
+async function getData() {
+  const response = await fetch("https://reqres.in/api/users/2");
+  const dataString = await response.text();
+  const data = JSON.parse(dataString);
+  console.log({response, dataString, data});
+}
+
+getData();
