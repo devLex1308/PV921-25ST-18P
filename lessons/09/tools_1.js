59612b8309a21990905fc879d855ed63772d0eec
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-12 18:52:27 +0300

    Add Promise

--- lessons/09/tools_1.js
+++ lessons/09/tools_1.js
@@ -1,24 +1,53 @@
-console.log(5);
+// console.log(5);
 
-const user = {
-  name: "Sasha",
-  age: 30,
-};
+// const user = {
+//   name: "Sasha",
+//   age: 30,
+// };
 
-const userString = `
-{
-  "name": "Sasha",
-  "age": 30,
-  "array": [8, 10, "text"],
-  "isUser": true
-}
-`;
+// const userString = `
+// {
+//   "name": "Sasha",
+//   "age": 30,
+//   "array": [8, 10, "text"],
+//   "isUser": true
+// }
+// `;
 
 
-try {
-  const obj = JSON.parse(userString);
-  console.log({obj});
-} catch(e) {
-  console.log(e);
-}
+// let userFromJSON = null;
+
+// try {
+//   let userFromJSON = JSON.parse(userString);
+//   console.log({userFromJSON});
+// } catch(e) {
+//   console.log(e);
+// }
+
+// if (userFromJSON) {
+//   console.log({userFromJSON});
+// }
+
+// const userToJSON = JSON.stringify(user);
+// console.log(userToJSON);
+
+
+
+const prom = new Promise((resolve, reject) => {
+  // reject(2018);
+  // const res = 2020;
+  // resolve(res);
+  // resolve(2019);
+
+  setTimeout(() => {
+    resolve("Test");
+  }, 2000 );
+});
+
+prom.then((r) => {
+  console.log(r);
+}).catch((c) => {
+  console.log({c});
+});
+
 
