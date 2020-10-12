8a4e6488ccc84b290d7bbf54b525ea74b061502f
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-12 18:33:34 +0300

    Додано try catch

--- lessons/09/tools_1.js
+++ lessons/09/tools_1.js
@@ -2,12 +2,23 @@ console.log(5);
 
 const user = {
   name: "Sasha",
-  age: 30
+  age: 30,
 };
 
 const userString = `
 {
   "name": "Sasha",
-  "age": 30
+  "age": 30,
+  "array": [8, 10, "text"],
+  "isUser": true
 }
 `;
+
+
+try {
+  const obj = JSON.parse(userString);
+  console.log({obj});
+} catch(e) {
+  console.log(e);
+}
+
