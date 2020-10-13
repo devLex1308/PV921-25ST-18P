1e68a6fd701c3ec32c4c4207327c98287a3fbf11
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-13 18:28:03 +0300

    Додано 10-й урок

---
+++ lessons/10/tools.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(10, 10, 100, 100);
+  console.log(canvas);
+});
