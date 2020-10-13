d970e17a82abf75213949fffc1ea7ca6712a090d
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-13 18:41:04 +0300

    Оголосили функцію для створення шахової дошки

--- lessons/10/tools.js
+++ lessons/10/tools.js
@@ -1,8 +1,25 @@
+const borderWidth = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
 
   ctx.fillStyle = "green";
   ctx.fillRect(10, 10, 100, 100);
-  console.log(canvas);
+
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(150, 10, 100, 100);
+
+  // console.log(canvas);
+  createChessBoard(ctx, 8);
 });
+
+
+function createChessBoard(ctx, count) {
+  const canvas = document.querySelector("#canvas");
+  const width = canvas.offsetWidth - borderWidth * 2;
+  const height = canvas.offsetHeight - borderWidth * 2;
+  console.log({ width, height });
+  // offsetWidth 
+}
