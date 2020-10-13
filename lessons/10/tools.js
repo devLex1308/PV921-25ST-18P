eab780a38616cb3baf5c34c8a2eaf640e94472c5
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-13 19:11:44 +0300

    Add brownianMotion function

--- lessons/10/tools.js
+++ lessons/10/tools.js
@@ -12,8 +12,50 @@ document.addEventListener("DOMContentLoaded", () => {
 //   ctx.fillRect(150, 10, 100, 100);
 
   // console.log(canvas);
-  createChessBoard(ctx, 8);
+
+  // ctx.moveTo(90, 130);
+  // ctx.lineTo(95, 25);
+  // ctx.lineTo(150, 80);
+  // ctx.lineTo(205, 25);
+  // ctx.lineTo(210, 130);
+  // ctx.strokeStyle = '#ff0000';
+  // ctx.lineWidth = 15;
+  // ctx.stroke();
+  // createChessBoard(ctx, 8);
+  brownianMotion(ctx)
 });
+
+function getSizes() {
+  const canvas = document.querySelector("#canvas");
+  const width = canvas.offsetWidth - borderWidth * 2;
+  const height = canvas.offsetHeight - borderWidth * 2;
+  return {
+    width,
+    height
+  }
+}
+
+function brownianMotion(ctx) {
+  const {width, height} = getSizes();
+  ctx.strokeStyle = '#ff0000';
+
+  let x = width / 2;
+  let y = height /2 ;
+
+  ctx.moveTo(x, y);
+
+  setInterval(() => {
+    x += 10;
+    y -= 10; 
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  } , 1000);
+
+}
+
+
+
+
 
 
 function createChessBoard(ctx, count) {
