150cd8a9e73b43aac832f0c67cd8fa787267c81a
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-13 18:49:06 +0300

    Реалізовано вивід одного рядка

--- lessons/10/tools.js
+++ lessons/10/tools.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 100, 100);
-
-
-  ctx.fillStyle = "red";
-  ctx.fillRect(150, 10, 100, 100);
+//   ctx.fillStyle = "green";
+//   ctx.fillRect(10, 10, 100, 100);
+// 
+// 
+//   ctx.fillStyle = "red";
+//   ctx.fillRect(150, 10, 100, 100);
 
   // console.log(canvas);
   createChessBoard(ctx, 8);
@@ -20,6 +20,28 @@ function createChessBoard(ctx, count) {
   const canvas = document.querySelector("#canvas");
   const width = canvas.offsetWidth - borderWidth * 2;
   const height = canvas.offsetHeight - borderWidth * 2;
+  
+  const widthBlock = width / count;
+  const heightBlock = height / count; 
+
+  let x = 0;
+  let y = 0;
+
+  const dx = widthBlock;
+  const dy = heightBlock;
+
+  let color = 'white';
+  let iterator = 0;
+  for(let x = 0; x < width; x+= dx) {
+    console.log({x});
+    ctx.fillStyle = iterator % 2 == 0
+      ? 'white' 
+      : 'black';
+    ctx.fillRect(x, y, widthBlock, heightBlock);
+    iterator++;
+  }
+
+
   console.log({ width, height });
   // offsetWidth 
 }
