c5850f69fd585d08c3c9e4c3615deb60befe68ef
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-15 19:47:12 +0300

    Витягнуто базові елементи

--- lessons/11/tools.js
+++ lessons/11/tools.js
@@ -1,4 +1,4 @@
-console.log("Робота з датою");
+// console.log("Робота з датою");
 
 
 // function User() {
@@ -11,7 +11,7 @@ console.log("Робота з датою");
 //   return "Гарний мій текст!!!";
 // }
 
-const date = new Date();
+// const date = new Date();
 
 // date.setDate();
 // date.setFullYear();
@@ -21,46 +21,46 @@ const date = new Date();
 // date.setMonth();
 
 
-
-
-const _date = date.getDate();
-const day = date.getDay();
-const fullyear = date.getFullYear();
-const hours = date.getHours();
-const milliseconds = date.getMilliseconds();
-const minutes = date.getMinutes();
-const month = date.getMonth();
-const seconds = date.getSeconds();
-const time = date.getTime();
-
-
-const Xmas95 = new Date('Augest 13, 1989 11:15:30');
-
-const diff = date - Xmas95;
-
-Xmas95.setDate(Xmas95.getDate() + 10000);
-
-console.log(Xmas95);
+// 
+// 
+// const _date = date.getDate();
+// const day = date.getDay();
+// const fullyear = date.getFullYear();
+// const hours = date.getHours();
+// const milliseconds = date.getMilliseconds();
+// const minutes = date.getMinutes();
+// const month = date.getMonth();
+// const seconds = date.getSeconds();
+// const time = date.getTime();
+// 
+// 
+// const Xmas95 = new Date('Augest 13, 1989 11:15:30');
+// 
+// const diff = date - Xmas95;
+// 
+// Xmas95.setDate(Xmas95.getDate() + 10000);
+// 
+// console.log(Xmas95);
 
 
 // console.log(user);
-
-console.log({
-  _date,
-  day,
-  fullyear,
-  hours,
-  milliseconds,
-  minutes,
-  month,
-  seconds,
-  time,
-});
-
-const days = diff / (1000 * 60 * 60 *24);
-const _minutes = diff / (1000 * 60 * 60);
-console.log(date, {diff, days, _minutes});
-
+// 
+// console.log({
+//   _date,
+//   day,
+//   fullyear,
+//   hours,
+//   milliseconds,
+//   minutes,
+//   month,
+//   seconds,
+//   time,
+// });
+// 
+// const days = diff / (1000 * 60 * 60 *24);
+// const _minutes = diff / (1000 * 60 * 60);
+// console.log(date, {diff, days, _minutes});
+// 
 
 
 /*
@@ -74,3 +74,25 @@ console.log(date, {diff, days, _minutes});
 /*
   Порахувати коли вам було чи буде 10 тисяч днів і 1 мільярд секунд
 */
+
+const startYearElement = document.querySelector("#startYear");
+const startMonthElement = document.querySelector("#startMonth");
+const startDayElement = document.querySelector("#startDay");
+const endYearElement = document.querySelector("#endYear");
+const endMonthElement = document.querySelector("#endMonth");
+const endDayElement = document.querySelector("#endDay");
+
+console.log({
+  startYearElement,
+  startMonthElement,
+  startDayElement,
+  endYearElement,
+  endMonthElement,
+  endDayElement,
+});
+
+
+
+
+
+
