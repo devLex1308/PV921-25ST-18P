4f2863ce38806145d31e2f182c61f63447ba1cdb
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-15 18:37:37 +0300

    Додано базові методи взаємодії з датою

--- lessons/11/tools.js
+++ lessons/11/tools.js
@@ -1,10 +1,9 @@
 console.log("Робота з датою");
 
-const date = new Date();
 
-function User() {
-
-}
+// function User() {
+// 
+// }
 
 // const user = new User();
 // user.name = "Петро";
@@ -12,6 +11,28 @@ function User() {
 //   return "Гарний мій текст!!!";
 // }
 
-console.log(user);
+const date = new Date();
 
-console.log(date);
+const _date = date.getDate();
+const day = date.getDay();
+const fullyear = date.getFullYear();
+const hours = date.getHours();
+const milliseconds = date.getMilliseconds();
+const minutes = date.getMinutes();
+const month = date.getMonth();
+const seconds = date.getSeconds();
+const time = date.getTime();
+
+// console.log(user);
+
+console.log({
+  _date,
+  day,
+  fullyear,
+  hours,
+  milliseconds,
+  minutes,
+  month,
+  seconds,
+  time,
+});
