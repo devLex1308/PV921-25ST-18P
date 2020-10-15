58ca65effe794f48670408fb27fe6c8ccf7ea222
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-15 18:29:46 +0300

    11 урок робота з датою

---
+++ lessons/11/tools.js
@@ -0,0 +1,17 @@
+console.log("Робота з датою");
+
+const date = new Date();
+
+function User() {
+
+}
+
+// const user = new User();
+// user.name = "Петро";
+// User.prototype.toString = function() {
+//   return "Гарний мій текст!!!";
+// }
+
+console.log(user);
+
+console.log(date);
