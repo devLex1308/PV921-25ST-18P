68dcd5eee7bbe83a12535898b4d57930db9f7e9c
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-19 20:21:35 +0300

    Додано відловлювання події на прильот і відльот мишки

--- lessons/12/tools2.js
+++ lessons/12/tools2.js
@@ -1,3 +1,20 @@
-$(document ).ready(function() {
+$(document).ready(function() {
   console.log("Браузер повністю завантажив код 2");
+
+  $("#game td").click(function (el) {
+    $(this).html("X");
+  }).on( "mouseenter", function() {
+    $( this ).css({
+      "background-color": "yellow",
+      "font-weight": "bolder"
+    });
+  })
+  .on( "mouseleave", function() {
+    var styles = {
+      backgroundColor : "",
+      fontWeight: ""
+    };
+    $( this ).css(styles);
+  });;
+
 });
