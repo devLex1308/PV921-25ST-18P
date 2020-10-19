56f258c7d3acf2bb3d3927e8bc6c9c8ef8c6afb9
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-19 22:06:20 +0300

    Додано домашну роботу 12 та код на хрестики нулі

--- lessons/12/tools2.js
+++ lessons/12/tools2.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
   console.log("Браузер повністю завантажив код 2");
 
+  $table = $("#game");
+
   $("#game td").click(function (el) {
-    $(this).html("X");
+    
+    if(!$(this).html()) {   
+      if($table.hasClass("zero")) {
+        $(this).html("X");
+        $table.removeClass("zero");
+      } else {
+        $(this).html("0");
+        $table.addClass("zero");
+      }
+    }
+
+
+
   }).on( "mouseenter", function() {
     $( this ).css({
       "background-color": "yellow",
