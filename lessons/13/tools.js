a642fbaee8c61a4eda1058a6b49b390c5f8564a5
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-20 19:07:20 +0300

    Зроблено перемикання між інпутами якщо корстувач у них щось ввів

--- lessons/13/tools.js
+++ lessons/13/tools.js
@@ -17,28 +17,58 @@ $(document).ready(function() {
     $blockForAnimateElement.html(i);
   });
 
-  const $userNameElement = $("#userName");
+  // const $userNameElement = $("#userName");
 
-  $userNameElement.focus(function(){
-    console.log("focus");
-    $(this).attr("value","")
-  });
+  // $userNameElement.focus(function(){
+  //   console.log("focus");
+  //   $(this).attr("value","")
+  // });
+  // $userNameElement.blur(function(){
+  //   console.log("blur");
+  //   const value = $(this).attr("value");
+  //   console.log({
+  //     value,
+  //     width: $(this).css("width")
+  //   });
+  //   $(this).attr("value","Введите ФИО");
+
+  //   $("#userSuname").focus();
+
+
+  // });
+  // $userNameElement.change(function(){
+  //   console.log("change");
+  //   // alert("Содержимое данного элемента было изменено.")
+  // });
+
+  $userNameElement = $("#userName");
+  $userSunameElement = $("#userSuname");
+  $middleNameElement = $("#middleName");
+
   $userNameElement.blur(function(){
-    console.log("blur");
-    const value = $(this).attr("value");
-    console.log({
-      value,
-      width: $(this).css("width")
-    });
-    $(this).attr("value","Введите ФИО");
-
-    $("#userSuname").focus();
-
-
+    const val = $(this).val();
+    if (val) {
+      $userSunameElement.focus();
+    }
   });
-  $userNameElement.change(function(){
-    console.log("change");
-    // alert("Содержимое данного элемента было изменено.")
+
+
+  $userSunameElement.blur(function(){
+    const val = $(this).val();
+    if (val) {
+      $middleNameElement.focus();
+    }
   });
+
+
+
+  // $par1 = $("#par1");
+
+
+  //  $par1.toggle(function(){
+  //    $par1.css("color","green");
+  //    $par1.css("font-size","10px");
+  //  });
+
 
 });
