c6375d270ec82cfc920aa802cd713d4c0d2f7974
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-20 18:33:40 +0300

    Робота з input jQuery

--- lessons/13/tools.js
+++ lessons/13/tools.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
   console.log("DOM is load!");
   const $blockForAnimateElement = $("#blockForAnimate");
 
+  let i = 0;
+
   $blockForAnimateElement.mouseover(function() {
     $blockForAnimateElement.css("background","green");
+  }).mouseout(function() {
+    $blockForAnimateElement.css("background","#888");
+  }).click(function(){
+    i++;
+    $blockForAnimateElement.html(i);
+  }).dblclick(function() {
+    i=-3;
+    $blockForAnimateElement.html(i);
   });
 
-  $blockForAnimateElement.mouseout(function() {
-    $blockForAnimateElement.css("background","#888");
-  });
+  $("#el1").focus(function(){$(this).attr("value","")});
+  $("#el1").blur(function(){$(this).attr("value","Введите ФИО")});
+  $("#el2").change(function(){ alert("Содержимое данного элемента было изменено.") });
 
 });
-
-// $("p").mouseover(function(){$("p").css("color","green")});
-// $("p").mouseout(function(){$("p").css("color","black")});
