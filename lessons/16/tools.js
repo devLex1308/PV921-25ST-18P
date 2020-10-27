a074b5702bb3976268ac1527a9f9aa024a828515
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-27 19:49:20 +0200

    ООП на прототипах

--- lessons/16/tools.js
+++ lessons/16/tools.js
@@ -1,11 +1,122 @@
-let animal = {
-  eats: true
+const animal = {
+  eats: true,
+  getEats: function () {
+    return this.eats;
+  }
 };
-// let rabbit = {
-//   jumps: true
+
+const rabbit = {
+  jumps: true,
+};
+
+
+const bird = {
+
+  singSong: true,
+  test: {
+    a: 2,
+    b: 3,
+  },
+
+  get singSong() {
+    // return this.singSong;
+  },
+
+  fly: function () {
+    console.log("Я можу літати!");
+    return 'Політ';
+  }
+}
+
+bird.__proto__ = animal;
+
+const fish = {
+  swim: function () {
+    console.log("Я можу плавати!");
+  }
+}
+
+fish.__proto__ = animal;
+
+const predator = {
+  hunting: function () {
+    console.log("Ідем на полювання!");
+  }
+}
+
+predator.__proto__ = animal;
+
+rabbit.__proto__ = animal;
+
+const sparrow = {};
+sparrow.__proto__ = bird;
+
+// sparrow.__proto__.eats = 5;
+
+// animal.__proto__ = sparrow;
+
+// const sparrow2 = {
+//   ...sparrow
 // };
 
-// rabbit.__proto__ = animal;
+let sparrow2 = Object.defineProperties({}, Object.getOwnPropertyDescriptors(sparrow));
+
+// console.log(
+//   sparrow,
+//   sparrow.eats,
+//   sparrow.fly(), {animal}, {sparrow2}, sparrow2.eats);
+
+rabbit.eats = false;
+
+const eagle = {
+  name: "Kesha" ,
+  __proto__: sparrow
+};
+
+// eagle.__proto__ = sparrow;
+
+const keys = Object.keys(eagle);
+
+const eagle2 = {};
+
+for (key in eagle) {
+  eagle2[key] = eagle[key];
+}
+  // console.log(key);
+
+eagle2.test.a = "Не кеша";
+
+
+function User() {
+  this.name = "Іван";
+  this.suname = "Котляревський";
+
+  this.getFullName = function() {
+    return `${this.name} ${this.suname}`;
+  }
+}
+
+
+const user = new User();
+
+function Admin() {
+  this.isAdmin  = true;
+}
+
+Admin.prototype = new User("Леся", "Українка");
+
+const admin = new Admin("Ярослав", "Мудрий");
+
+console.log(user.getFullName(), admin.getFullName());
+
+
+
+// console.log({ eagle, eagle2 }, eagle.test.c);
+
+
+// console.log({animal, rabbit}, animal.getEats(), rabbit.getEats());
+
+
 
 // for (k in rabbit) {
 //   console.log(k);
