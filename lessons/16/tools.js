c67b5174646bcc2fd9b762d153b0b8ebb6be3308
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-10-27 18:26:44 +0200

    Початок 16 го уроку

---
+++ lessons/16/tools.js
@@ -0,0 +1,24 @@
+let animal = {
+  eats: true
+};
+// let rabbit = {
+//   jumps: true
+// };
+
+// rabbit.__proto__ = animal;
+
+// for (k in rabbit) {
+//   console.log(k);
+// }
+
+// console.log({rabbit});
+
+/*
+
+Object.keys(obj) / Object.values(obj) / Object.entries(obj) – возвращают массив всех перечисляемых собственных строковых ключей/значений/пар ключ-значение.
+Object.getOwnPropertySymbols(obj) – возвращает массив всех собственных символьных ключей.
+Object.getOwnPropertyNames(obj) – возвращает массив всех собственных строковых ключей.
+Reflect.ownKeys(obj) – возвращает массив всех собственных ключей.
+obj.hasOwnProperty(key): возвращает true, если у obj есть собственное (не унаследованное) свойство с именем key.
+
+ */
