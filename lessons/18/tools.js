a963108c0c67ed5de6fa242f53188f44cb761179
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-02 18:38:40 +0200

    Робота з глобальним символом

--- lessons/18/tools.js
+++ lessons/18/tools.js
@@ -8,10 +8,19 @@ const user = {
   [s1]: 100,
 };
 
+
+const userString = JSON.stringify(user);
+
 const userClone = {
   ...user,
 }
 
+const s3 = Symbol.for("global symbol");
+const s4 = Symbol.for("global symbol");
+const s5 = Symbol.for("global symbol 5");
 
-console.log({user, userClone});
-console.log(user[s1]);
+
+console.log({user, userClone, s3}, s3 == s4, s3 == s5);
+console.log(user[Symbol("start")]);
+
+// alert(userString);
