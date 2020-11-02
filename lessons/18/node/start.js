07c37328a54955dc1871fc7d08fab12357914e2e
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-02 20:07:58 +0200

    Import and export робота з консолькою

--- lessons/18/node/start.js
+++ lessons/18/node/start.js
@@ -1,6 +1,10 @@
-console.log("This is nodeJs!");
+const {sum, test} = require('./sum.js');
 
-console.log(typeof window);
+// console.log(result);
+
+// console.log("This is nodeJs!");
+
+// console.log(typeof window);
 
 /*
 Написати функцію checkEnviroment яка повертає значення
@@ -10,9 +14,19 @@ console.log(typeof window);
  */
 
 
-try {
-  console.log(process);
 
-} catch (e) {
-  // console.log(e);
-}
+// const [urlNode, srcFile, ...arg] = process.argv;
+const [urlNode, srcFile, variable1, variable2] = process.argv;
+
+console.log(sum(variable1, variable2));
+// for (let i = 0; i < arg.length; i++ ) {
+//   const name = arg[i];
+//   console.log(`Привіт ${name}`);
+// }
+
+
+// try {
+
+// } catch (e) {
+//   // console.log(e);
+// }
