e2f940e554f32d3ecbd4324999c04004eaabc020
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-03 19:13:01 +0200

    Збереження користувачів що запускали програму

--- lessons/19-server-node/savesUsers.js
+++ lessons/19-server-node/savesUsers.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const fileName = "./users.txt";
 
+let storage = {};
 
 fs.readFile(fileName, "utf-8", function(err, data) {
 
@@ -9,19 +10,26 @@ fs.readFile(fileName, "utf-8", function(err, data) {
 
   if (err) {
     // console.log(err);
-    return;
+    storage.users = [];
+    // return;
+  } else {
+    storage = JSON.parse(data);
   }
 
+  storage.users.forEach(item => {
+    console.log(`Нас відвідав(ла) ${item}`)
+  });
 
+  storage.users.push(user);
 
-  fs.writeFile(fileName, `${data}${data}`, function(err, data2) {
+  fs.writeFile(fileName, JSON.stringify(storage), function(err, data2) {
     if (err) {
       console.log(err);
       return;
     }
-    console.log({data2});
+    // console.log({data2});
   });
 
-  console.log({data});
+  // console.log({data});
 });
 
