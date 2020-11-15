ff9412b8f338f818f423b3a0bb10263884ba8a67
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-15 13:58:15 +0200

    Наведено як кодити без jsx

---
+++ lessons/23-next-js/pages/24/example/notUseJsx.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// https://ru.reactjs.org/docs/react-without-jsx.html
+// https://ru.reactjs.org/docs/jsx-in-depth.html
+
+export default function App() {
+  return React.createElement("p", null, "Nothing will come of nothing");
+}
