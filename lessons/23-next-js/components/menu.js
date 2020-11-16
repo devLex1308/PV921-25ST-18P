017d598bea72c2d338f63d06c860a9aff08fcbfa
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-16 18:42:45 +0200

    Додано меню

---
+++ lessons/23-next-js/components/menu.js
@@ -0,0 +1,18 @@
+function Menu(props) {
+
+  return (
+    <ul>
+      <li><a href="/24/example/ButtonClickHandler">ButtonClickHandler</a></li>
+      <li><a href="/24/example/ClassDateTime">ClassDateTime</a></li>
+      <li><a href="/24/example/CurrentTime">CurrentTime</a></li>
+      <li><a href="/24/example/DateAndTime">DateAndTime</a></li>
+      <li><a href="/24/example/FirstClassComponent">FirstClassComponent</a></li>
+      <li><a href="/24/example/FirstClassComponent">FirstClassComponent</a></li>
+      <li><a href="/24/example/">index</a></li>
+      <li><a href="/24/example/notUseJsx">notUseJsx</a></li>
+      <li><a href="/24/example/Quote">Quote</a></li>
+    </ul>
+  );
+}
+
+export default Menu;
--- lessons/23-next-js/pages/24/example/index.js
+++ lessons/23-next-js/pages/24/example/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 // import "./styles.css";
 
+import Menu from "../../../components/menu.js"
+
 
 // https://ru.reactjs.org/docs/getting-started.html
 // https://ru.reactjs.org/tutorial/tutorial.html
@@ -9,16 +11,17 @@ export default function App(props) {
   /*
   деструктурируем объект props
   */
-  let { name, lastName } = props;
+  let { name = "Тарас", lastName = "Шевченко" } = props;
   return (
     <div className="App">
+      <Menu />
       <h1>Information about writer:</h1>
       <h2>{name}</h2>
       <h2>{lastName}</h2>
     </div>
   );
 
-  /*  
+  /*
 Альтернативная форма деструктуризации
   let { name: n, lastName: l } = props;
   return (
