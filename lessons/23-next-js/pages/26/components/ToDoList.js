9af32546674ef5b78a64bb730fd42a56036529fd
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-19 19:55:02 +0200

    Базова логіка на активовану задачу

--- lessons/23-next-js/pages/26/components/ToDoList.js
+++ lessons/23-next-js/pages/26/components/ToDoList.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 export default class ToDoList extends React.Component{
+  state = {
+    activeToDoId: null,
+  }
+
   render() {
     const { todos } = this.props;
     return(
       <ul>
         {
           todos.map(todo => (
-            <li key={todo.id}>{todo.name}</li>
+            <li key={todo.id}>
+              {todo.name}
+              {
+                this.state.activeToDoId == todo.id ? (
+                  виводимо опис
+                ) : null
+              }
+            </li>
           ))
         }
       </ul>
