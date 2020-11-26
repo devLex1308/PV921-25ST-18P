33128809f2d121f6bd8f4eb792a5e408f11db8a9
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-26 19:15:33 +0200

    Додано useEffect

--- lessons/23-next-js/pages/28/index.js
+++ lessons/23-next-js/pages/28/index.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
 export default function app() {
 
-  const [count, setCount] = useState(0);
+   // Аналогично componentDidMount и componentDidUpdate:
+  const [count, setCount] = useState(5000);
+  const [count1, setCount1] = useState(5000);
+  useEffect(() => {
+    // Обновляем заголовок документа с помощью API браузера
+    document.title = `Вы нажали ${count} раз`;
+    console.log("useEffect");
+  },[0]);
+
+  console.log("render");
   return (
     <>
       <h2>Урок 28</h2>
       <button onClick={() => setCount(count + 1)}>
       {count}
+      </button>
+      <button onClick={() => setCount1(count1 + 1)}>
+      {count1}
       </button>
     </>
   );
