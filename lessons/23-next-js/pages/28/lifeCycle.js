da81200827fca6dbec125d08a30b375574c407a6
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-26 18:55:04 +0200

    Додано кнопку щоб досліджувати зміну станів

--- lessons/23-next-js/pages/28/lifeCycle.js
+++ lessons/23-next-js/pages/28/lifeCycle.js
@@ -4,6 +4,10 @@ import React from "react";
 
 export default class app extends React.Component {
 
+  state = {
+    count: 0,
+  }
+
   constructor(props) {
     super(props);
     console.log("constructor", {props});
@@ -14,7 +18,8 @@ export default class app extends React.Component {
   // }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldComponentUpdate", {props, state});
+    console.log("shouldComponentUpdate", {nextProps, nextState});
+    return true;
   }
 
   componentDidMount(props) {
@@ -22,13 +27,18 @@ export default class app extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("componentDidMount", {prevProps, prevState, snapshot});
+    console.log("componentDidUpdate", {prevProps, prevState, snapshot});
   }
 
   render (props) {
-    console.log("constructor", {props});
+    console.log("render", {props});
     return (
-      <h2>Урок 28</h2>
+      <>
+        <h2>Урок 28</h2>
+        <button onClick={() => this.setState({count: this.state.count + 1})}>
+        {this.state.count}
+        </button>
+      </>
     );
   }
 }
