1e68c8c7f2a48abd57a6eed0e10608b6cdda5569
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-11-29 23:10:16 +0200

    Binding

--- lessons/29-angular/angular/src/app/app.component.ts
+++ lessons/29-angular/angular/src/app/app.component.ts
@@ -13,4 +13,15 @@ export class AppComponent {
 
   obj = { a: 1, b: {c: 2} }
 
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsm-dQHFmWp9Xw1e-4BfLDr67vBq5cil6OytRJExumqHUzTHVZ'
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over')
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, 5000)
+  }
+
+
+
 }
