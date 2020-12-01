e948dfc3eec7ed62a593e5e3b0b3a5c6605c7bff
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-01 19:18:17 +0200

    Зробили кілька компонентів

--- lessons/29-angular/my-app/src/app/app.module.ts
+++ lessons/29-angular/my-app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppPost } from './post/post.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppPost,
+    Post2Component,
+    Post3Component,
   ],
   imports: [
     BrowserModule
---
+++ lessons/29-angular/my-app/src/app/post3/post3.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post3',
+  template: `
+  	<p class="myP">Компонент в одному файлі</p>
+  `,
+  styles: [`
+  .myP {
+  	color: red,
+  }
+  `]
+})
+export class Post3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
