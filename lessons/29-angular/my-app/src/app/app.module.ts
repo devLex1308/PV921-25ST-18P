17ce5c2ff4f2ce609a79011256c6319b8fb17dc8
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-01 18:55:35 +0200

    Запускаємо ангуляр

--- lessons/29-angular/my-app/src/app/app.module.ts
+++ lessons/29-angular/my-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppPost } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppPost,
   ],
   imports: [
     BrowserModule
