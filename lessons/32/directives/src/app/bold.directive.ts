56757c69f1991e3b11ab08945b119a3601910dd5
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-07 21:00:58 +0200

    Робимо директиви з прокидуванням параметрів

--- lessons/32/directives/src/app/bold.directive.ts
+++ lessons/32/directives/src/app/bold.directive.ts
@@ -1,13 +1,51 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBold]'
 })
 export class BoldDirective {
 
-  	constructor(private elementRef: ElementRef){
+  	@Input() selectedSize = "18px";
+    @Input() defaultSize = "16px";
+    @Input() selectColor = "#ff1187";
+     
+    private defaultColor:string = "black";
+    private fontSize : string;
+    private fontWeight = "normal";
+    private color = "black";
+    ngOnInit(){
+        this.fontSize = this.defaultSize;
+    }
+     
+    @HostBinding("style.fontSize") get getFontSize(){
          
-    	this.elementRef.nativeElement.style.fontWeight = "bold";
-	}
+        return this.fontSize;
+    }
+     
+    @HostBinding("style.fontWeight") get getFontWeight(){
+         
+        return this.fontWeight;
+    }
+
+    @HostBinding("style.color") get getColor(){
+         
+        return this.color;
+    }
+     
+    @HostBinding("style.cursor") get getCursor(){
+        return "pointer";
+    }
+     
+    @HostListener("mouseenter") onMouseEnter() {
+        this.fontWeight ="bold";
+        this.fontSize = this.selectedSize;
+        this.color = this.selectColor;
+    }
+ 
+    @HostListener("mouseleave") onMouseLeave() {
+        this.fontWeight = "normal";
+        this.fontSize = this.defaultSize;
+        this.color = this.defaultColor;
+    }
 
 }
