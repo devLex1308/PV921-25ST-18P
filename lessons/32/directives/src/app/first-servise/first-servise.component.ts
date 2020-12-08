17f9344ef00df6aa517105084596dc917127487d
Nomeit <kartoshkamamasha@gmail.com>
2020-12-08 19:16:46 +0200

    fix bags

--- lessons/32/directives/src/app/first-servise/first-servise.component.ts
+++ lessons/32/directives/src/app/first-servise/first-servise.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-first-servise',
   templateUrl: './first-servise.component.html',
-  styleUrls: ['./first-servise.component.css']
+  styleUrls: ['./first-servise.component.css'],
+  providers: [DataService]
 })
 export class FirstServiseComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  items: string[] = [];
+  name: string;
+  constructor(private dataService: DataService){}
+    
+  addItem(name: string){
+  	let newName = name;
+  	if (newName && newName.trim())
+    	this.dataService.addData(newName);
   }
-
+  ngOnInit(){
+    this.items = this.dataService.getData();
+  }
 }
