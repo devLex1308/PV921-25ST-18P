d499b308a5af053efbdcebc89a9827090b74d2e0
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-08 19:17:15 +0200

    Підключили перший сервіс

--- lessons/32/directives/src/app/first-servise/first-servise.component.ts
+++ lessons/32/directives/src/app/first-servise/first-servise.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-first-servise',
   templateUrl: './first-servise.component.html',
-  styleUrls: ['./first-servise.component.css']
+  styleUrls: ['./first-servise.component.css'],
+  providers: [DataService],
 })
 export class FirstServiseComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+   	items: string[] = [];
+    name: string;
+    constructor(private dataService: DataService){}
+      
+    addItem(name: string){
+          
+        this.dataService.addData(name);
+    }
+    ngOnInit(){
+        this.items = this.dataService.getData();
+    }
 
 }
