0ab8ee8837bab9364f871ba7f0c264fc79a9eae5
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-10 18:48:53 +0200

    Додано модуль для прикладу роботи з формами

---
+++ lessons/schedule/src/app/exapmle/exapmle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exapmle',
+  templateUrl: './exapmle.component.html',
+  styleUrls: ['./exapmle.component.css']
+})
+export class ExapmleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
