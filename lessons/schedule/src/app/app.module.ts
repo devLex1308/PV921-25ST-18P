6007781426a95638ee9adb365ab23f4dec3225c3
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-10 20:08:31 +0200

    Додаємо запити на сервер

--- lessons/schedule/src/app/app.module.ts
+++ lessons/schedule/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExapmleComponent } from './exapmle/exapmle.component';
+import { HttpComponent } from './http/http.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExapmleComponent
+    ExapmleComponent,
+    HttpComponent
   ],
   imports: [
     BrowserModule,
