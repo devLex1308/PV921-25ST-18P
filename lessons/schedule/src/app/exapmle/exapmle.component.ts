c952dbf6b977bb0de32e16c19b88ee8128f31ca0
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-10 18:57:07 +0200

    Додаємо форму з телефоном

--- lessons/schedule/src/app/exapmle/exapmle.component.ts
+++ lessons/schedule/src/app/exapmle/exapmle.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+export class Phone{
+    constructor(public title: string, 
+                public price: number, 
+                public company: string)
+    { }
+}
+
 
 @Component({
   selector: 'app-exapmle',
   templateUrl: './exapmle.component.html',
   styleUrls: ['./exapmle.component.css']
 })
-export class ExapmleComponent implements OnInit {
+export class ExapmleComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  title: string;
+	price: number;
+	company: string;
+	 
+	phones: Phone[] = [];
+	companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
+	 
+	addPhone(){
+	    this.phones.push(new Phone(this.title, this.price, this.company));
+	}
 
 }
