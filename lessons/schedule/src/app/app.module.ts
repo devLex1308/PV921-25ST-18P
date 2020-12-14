468be524928415dc3c4440ac82c2ef322ca95952
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-14 18:43:48 +0200

    Зроблено новий модуль в якому будемо робити конвертор валют

--- lessons/schedule/src/app/app.module.ts
+++ lessons/schedule/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule }   from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExapmleComponent } from './exapmle/exapmle.component';
+// import { ExapmleComponent } from './exapmle/exapmle.component';
 import { HttpComponent } from './http/http.component';
 
-import { HttpClientModule }   from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExapmleComponent,
+    // ExapmleComponent,
     HttpComponent
   ],
   imports: [
