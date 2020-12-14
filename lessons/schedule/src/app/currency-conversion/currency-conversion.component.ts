19ae548d1a1bcff2bb216c86690162a6b07a3ef0
oleksandr <oleksandr.pavliuk1308@gmail.com>
2020-12-14 19:26:27 +0200

    Створено селектор з валютами та інізіалізовано початкові дані

--- lessons/schedule/src/app/currency-conversion/currency-conversion.component.ts
+++ lessons/schedule/src/app/currency-conversion/currency-conversion.component.ts
@@ -10,9 +10,24 @@ export class CurrencyConversionComponent implements OnInit {
 
   constructor(private http: HttpClient){}
 
+  currentCurrency: string = '';
+
+  currentBuy: number = 0;
+  currentSale: number = 0;
+
+
   ngOnInit(): void {
   	this.http.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5').subscribe((data: User) => {
-    	this.user = data;
+    	this.dataArray = data;
+
+      const {
+        ccy, buy, sale
+      } = data[0];
+      this.currentCurrency = ccy;
+
+
+      this.currentBuy = buy;
+      this.currentSale = sale;
     	console.log({data});
     });
 
